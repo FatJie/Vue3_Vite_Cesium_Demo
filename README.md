d94012a60e9ce40c3fcc49ac4b020f24a4fd2d16
Fatjie <372761781@qq.com>
2023-09-02 15:57:01 +0800

    init cesium map

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vue 3 + Vite + Cesium Demo
+
+```
+1、pnpm create vite demo (vue + js)
+2、pnpm i
+3、pnpm i cesium@1.99 vite-plugin-cesium
+```
+
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="cesiumContainer">
+  </div>
+</template>
+
+<script setup>
+import * as Cesium from 'cesium'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyNjAzZTE3OS1lNzVmLTQ3NmUtODQyMC02MTdiOTIwYzBiOWYiLCJpZCI6MTY0MDMwLCJpYXQiOjE2OTM2MzY1NjZ9.9O-QAxJzRmI2LsaBAct4Qud1ITKzaY5fLFNp2MNt-0Y'
+  new Cesium.Viewer('cesiumContainer')
+
+})
+
+</script>
+
+<style scoped>
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
